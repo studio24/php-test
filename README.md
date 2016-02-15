8a5ba8a189b1c065f4c6cacb33a83b789d178c67
Simon R Jones <simon@studio24.net>
2016-02-15 22:01:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# PHP Test
+
+The following are a set of two developer tests that we ask prospective staff to complete before face-to-face interviews. 
+They are intended to assess skills in PHP.
+
+Please ensure you send us all files required to run the scripts. We will test your submission using PHP 5.6
+
+You are free to send multiple files to answer the questions but all files created to answer this test must be created 
+within the time allocated.
+
+You are not allowed to use any external libraries or code snippets (i.e. no jQuery, web frameworks or any pre-written 
+libraries you may have). The only exception to this is you may use a testing framework for question 2 if you wish 
+(e.g. PHPUnit).
+
+Please answer as many of the questions as you can within 3 hours.
+
+# 1 – Multidimensional array
+
+Display the list of books in an HTML page based on the data array in _data.php_, outputting all content in the data 
+array to the page. 
+
+You should include functionality on the page that allows the user to sort the data by:
+ 
+* Name (ascending)
+* Price (cheapest first)
+
+And filter the data by:
+
+* Only show books released in last 3 months
+* Only show books released in last 6 months
+* Show all books
+
+By default display all books, sorted by Price (cheapest first).
+
+The format of your answer should be a PHP script (to be accessed via a web browser) that outputs a valid HTML document. 
+The primary focus of the test is PHP, so please don’t spend lots of time crafting beautiful CSS – it’s not necessary. 
+However, it is expected that you use appropriate and clean HTML to output results.
+￼￼￼￼￼￼￼￼
+# 2 - OO
+
+Write a number of PHP classes to represent the following business rules:
+
+* Each employee is either a Manager or Staff
+* All Staff have a Staff ID, Email address and Password
+* Staff ID cannot be changed once set
+* Managers also have a Telephone number
+
+Please test these business rules using the following tests. At their simplest the test should be a single PHP script
+that contains code to run these tests. You may use a testing framework (e.g. PHPUnit) for this element if you wish.  
+
+* I can create an employee setting ID and email
+* I can only set a valid email address for an employee
+* I cannot change the Staff ID once set
+* I can set a password and this is stored securely (e.g. I cannot return the password in plain text)
+* I can check to see if the stored password is the same as an inputted password
+* I can only set a Telephone number on a Manager
+* I can test whether an employee is a Manager
+
+The format of your answer can either be a PHP script run on the CLI, or a PHP script (to be accessed via a web browser) 
+that outputs a valid HTML document.
+￼￼
---
+++ data.php
@@ -0,0 +1,68 @@
+<?php
+
+$data = [
+    0 => [
+        'title' => "How the Marquis Got His Coat Back",
+        'author' => "Neil Gaiman",
+        'price' => "1.59",
+        'releaseDate' => "2015-10-27",        
+        'href' => "http://www.amazon.co.uk/gp/product/1472235320",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/51Cbka5czLL._AA160_.jpg",
+    ],
+    1 => [
+        'title' => "Never Let Me Go",
+        'author' => "Kazuo Ishiguro",
+        'price' => "6.74",
+        'releaseDate' => "2010-02-25",
+        'href' => "http://www.amazon.co.uk/Never-Let-Me-Kazuo-Ishiguro/dp/0571258093",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/5102qi5mt5L._AA160_.jpg",
+    ],
+    2 => [
+        'title' => "The Ladybird Book of the Hipster",
+        'author' => "Joel Morris",
+        'price' => "5.24",
+        'releaseDate' => "2015-10-29",
+        'href' => "http://www.amazon.co.uk/gp/product/0718183592",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/51QUA1gCnNL._AA160_.jpg",
+    ],
+    3 => [
+        'title' => "Career of Evil",
+        'author' => "Robert Galbraith",
+        'price' => "7.99",
+        'releaseDate' => "2015-10-20",
+        'href' => "http://www.amazon.co.uk/gp/product/0751562270",
+        'imageSrc' => "https://images-eu.ssl-images-amazon.com/images/I/51t%2BVGJ5riL._AA160_.jpg",
+    ],
+    4 => [
+        'title' => "SPQR: A history of Ancient Rome",
+        'author' => "Professor Mary Beard",
+        'price' => "9.99",
+        'releaseDate' => "2015-10-20",
+        'href' => "http://www.amazon.co.uk/gp/product/1846683807",
+        'imageSrc' => "https://images-eu.ssl-images-amazon.com/images/I/41xDARHqn-L._AA160_.jpg",
+    ],
+    5 => [
+        'title' => "How It Works: The Mum (Ladybirds for Grown-Ups)",
+        'author' => "Jason Hazeley, Joel Morris",
+        'price' => "3.49",
+        'releaseDate' => "2016-02-11",
+        'href' => "http://www.amazon.co.uk/How-Works-Mum-Ladybirds-Grown-Ups/dp/0718184211",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/51M4XrArwrL._AA160_.jpg",    
+    ],
+    6 => [
+        'title' => "The Girl Who Kicked the Hornets' Nest",
+        'author' => "Stieg Larsson",
+        'price' => "4.00",
+        'releaseDate' => "2015-06-04",
+        'href' => "http://www.amazon.co.uk/Girl-Kicked-Hornets-Nest-Millennium/dp/0857054058",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/51MRzWAHAWL._AA160_.jpg",
+    ],
+    7 => [
+        'title' => "The 24-Hour Wine Expert",
+        'author' => "Jancis Robinson",
+        'price' => "2.99",
+        'releaseDate' => "2016-02-04",
+        'href' => "http://www.amazon.co.uk/24-Hour-Wine-Expert-Jancis-Robinson/dp/0141981814",
+        'imageSrc' => "http://ecx.images-amazon.com/images/I/61OvFTH+wfL._AA160_.jpg"
+    ]
+];
